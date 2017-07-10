e2bf3ce528fec325ab0ee733b6ccf4280f24c996
ashfaaq77 <ashfaaq77@live.co.uk>
2017-07-10 14:33:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -37,11 +37,9 @@ def webhook():
 
 
 def processRequest(req):
-    print("asnfn")
-   #lol = req.get("result").get("interaction").get("action")
-   # print(lol)
-   if req.get("result").get("interaction").get("action") != "yahooWeatherForecast":
-    return {}
+     if req.get("result").get("action") != "yahooWeatherForecast":
+        return {}
+    
     print("test passed")
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
